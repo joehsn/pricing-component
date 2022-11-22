e212a393e6a8fd47cb9aff269ebb15bf802e4c9d
Joe-Hsn <youssef.hassan.mahmoud@gmail.com>
2022-11-22 20:12:16 +0200

    sat up

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // ### Primary
+        FLPG: "hsl(236, 72%, 79%)",
+        TFLPG: "hsl(237, 63%, 64%)",
+        // ### Neutral
+        "very-light-grayish-blue": "hsl(240, 78%, 98%)",
+        "light-grayish-blue": "hsl(234, 14%, 74%)",
+        "grayish-blue": "hsl(233, 13%, 49%)",
+        "dark-grayish-blue": "hsl(232, 13%, 33%)",
+      },
+    },
+  },
+  plugins: [],
+};
